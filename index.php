24b2bb932aa8bd8bdc41f34649cfd5577ca750c9
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-02-09 17:46:38 +0800

    modified index.php for frontend

--- index.php
+++ index.php
@@ -8,10 +8,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Home</title>
 </head>
-<body>
-    <!-- proceeds to student info form page -->
-    <a href="student_info.php"><button>Submit a Request</button></a>
+<body> 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-container custom-margin"> 
+                    <form class="submit-form">
+                        <div class="form-group">
+                            <label for="request-type">Completion Form</label>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="request-type" id="request-type-1" value="option1" checked>
+                                <label class="form-check-label" for="request-type-1">Late Reporting of Grades</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="request-type" id="request-type-2" value="option2">
+                                <label class="form-check-label" for="request-type-2">Correction of Entry</label>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="request-type" id="request-type-3" value="option3">
+                                <label class="form-check-label" for="request-type-3">Completion of Incomplete Grades</label>
+                            </div>
+                        </div>
+                        <div class="mb-3"></div>
+                        <a href="student_info.php" class="btn btn-primary submit-request-button">SUBMIT A REQUEST</a>
+                    </form>
+                </div>
+            </div>
+        </div>  
 </body>
 </html>
