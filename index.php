c5695daf423b63b36410aef18c7759d4d342a7f1
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-02-09 20:17:07 +0800

    index.php radio b into lists

--- index.php
+++ index.php
@@ -16,28 +16,18 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <div class="form-container custom-margin"> 
-                    <form class="submit-form">
-                        <div class="form-group">
-                            <label for="request-type">Completion Form</label>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="request-type" id="request-type-1" value="option1" checked>
-                                <label class="form-check-label" for="request-type-1">Late Reporting of Grades</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="request-type" id="request-type-2" value="option2">
-                                <label class="form-check-label" for="request-type-2">Correction of Entry</label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio" name="request-type" id="request-type-3" value="option3">
-                                <label class="form-check-label" for="request-type-3">Completion of Incomplete Grades</label>
-                            </div>
-                        </div>
-                        <div class="mb-3"></div>
-                        <a href="student_info.php" class="btn btn-primary submit-request-button">SUBMIT A REQUEST</a>
-                    </form>
-                </div>
+                 <form>
+                    <label for="request-type">Completion Form</label>
+                     <ul class="list-unstyled list-bullet">
+                        <li class="list-group-item">Late Reporting of Grades</li>
+                        <li class="list-group-item">Correction of Entry</li>
+                        <li class="list-group-item">Completion of Incomplete Grades</li>
+                    </ul>
+                    <div class="mb-3"></div>
+                    <a href="student_info.php" class="btn btn-primary submit-request-button">SUBMIT A REQUEST</a>
+                </form>
             </div>
-        </div>  
+        </div>
+    </div>  
 </body>
 </html>
