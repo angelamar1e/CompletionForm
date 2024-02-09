a937fec68e47074b182029085e58956c42d72d76
Aron Nick Santos <aronnicksnts@gmail.com>
2024-02-09 10:26:54 +0800

    index and navbar added

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+    include('navbar.html');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    <!-- proceeds to student info form page -->
+    <a href="student_info.php"><button>Submit a Request</button></a>
+</body>
+</html>
