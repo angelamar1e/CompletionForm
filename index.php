6a0e31599906b0e5768825298e24aff74e7e312a
angelamar1e <angela.delapena06@gmail.com>
2024-02-11 10:21:20 +0800

    improved navbar and index

--- index.php
+++ index.php
@@ -12,12 +12,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Home</title>
 </head>
-<body> 
+<body class="custom-font vh-100">
     <div class="container">
         <div class="row">
             <div class="col-md-6">
                  <form>
-                    <label for="request-type">Completion Form</label>
+                    <label for="request-type">Completion Form</label><br>
                      <ul class="list-unstyled list-bullet">
                         <li class="list-group-item">Late Reporting of Grades</li>
                         <li class="list-group-item">Correction of Entry</li>
